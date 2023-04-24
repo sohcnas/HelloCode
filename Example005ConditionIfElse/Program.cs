e3f42575863e553353f6a6bdb505504c2d17f9b0
Alexandr Taravkov <sohcnas@yandex.ru>
2023-04-24 16:55:28 +0300

    Example 005 Condition IF Else

---
+++ Example005ConditionIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это МАША");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
+
+
+
